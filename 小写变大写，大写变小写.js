2f35452c247f36c48efb3e3c91e51c883a832415
189690 <2312165827@qq.com>
2023-08-01 16:13:01 +0800

    修改

--- 小写变大写，大写变小写.js
+++ 小写变大写，大写变小写.js
@@ -1,21 +1,34 @@
 let str = 'ab cD'
 
 function fn(str) {
-  let res = ''
-  for (const s of str) {
-    res += change(s)
-  }
-  function change(s) {
-    if (s.charCodeAt() >= 97 && s.charCodeAt() <= 122) {
-      return s.toUpperCase()
-    } else if (s.charCodeAt() >= 65 && s.charCodeAt() <= 90) {
-      return s.toLowerCase()
-    } else {
-      return s
-    }
-  }
-  return res
+  return str.replace(/[a-zA-Z]/g, (a, index, c) => {
+    return change(a);
+  });
 }
+
+function change(s) {
+  return s.charCodeAt() >= 97 && s.charCodeAt() <= 122
+    ? s.toUpperCase()
+    : s.toLowerCase();
+}
+
+
+// function fn(str) {
+//   let res = ''
+//   for (const s of str) {
+//     res += change(s)
+//   }
+//   function change(s) {
+//     if (s.charCodeAt() >= 97 && s.charCodeAt() <= 122) {
+//       return s.toUpperCase()
+//     } else if (s.charCodeAt() >= 65 && s.charCodeAt() <= 90) {
+//       return s.toLowerCase()
+//     } else {
+//       return s
+//     }
+//   }
+//   return res
+// }
 console.log(fn(str));
 console.log('A'.charCodeAt()); // 65
 console.log('Z'.charCodeAt()); // 90
