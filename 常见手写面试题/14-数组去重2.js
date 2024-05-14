0c3e7af6bf8dc30b1415f2349704e630ca02e871
189690 <yuhang.ma@cn.chervongroup.com>
2024-05-14 13:58:47 +0800

    feat: 修改数组去重2
    1.对象属性相同时也算相同

--- 常见手写面试题/14-数组去重2.js
+++ 常见手写面试题/14-数组去重2.js
@@ -1,7 +1,40 @@
 // 数组去重(如果对象的属性和值都相等，则看做这个对象相等)
-let arr = ['a', { a: 1 }, { a: 1 }, 'a', 1, 1]
+let arr = [
+  'a',
+  { a: 1, b: undefined },
+  [1, 2],
+  [1, 2],
+  { a: 1, c: undefined },
+  'a',
+  1,
+  1,
+]
 
-function fn(arr) {
+function isObject(val) {
+  // return typeof val === 'object' && val !== null
+  return val === Object(val)
+}
+
+function equals(val1, val2) {
+  if (isObject(val1) && isObject(val2)) {
+    if (Object.keys(val1).length !== Object.keys(val2).length) {
+      return false
+    }
+    for (const key in val1) {
+      if (!Object.keys(val2).includes(key)) {
+        return false
+      }
+      if (!equals(val1[key], val2[key])) {
+        return false
+      }
+    }
+    return true
+  } else {
+    return Object.is(val1, val2)
+  }
+}
+
+function fn1(arr) {
   let newArr = [...arr]
   for (let i = 0; i < newArr.length; i++) {
     for (let j = i + 1; j < newArr.length; j++) {
@@ -11,29 +44,22 @@ function fn(arr) {
       }
     }
   }
-  function equals(val1, val2) {
-    if (
-      typeof val1 === 'object' &&
-      val1 !== null &&
-      typeof val2 === 'object' &&
-      val2 !== null
-    ) {
-      if (Object.keys(val1).length !== Object.keys(val2).length) {
-        return false
-      }
-      for (const key in val1) {
-        if (!Object.keys(val2).includes(key)) {
-          return false
-        }
-        if (!equals(val1[key], val2[key])) {
-          return false
-        }
-      }
-      return true
-    } else {
-      return val1 === val2
-    }
-  }
   return newArr
 }
-console.log(fn(arr))
+
+function fn2(arr) {
+  const result = []
+  // 为外层循环打上标记（原生js就有的语法）
+  outer: for (const item of arr) {
+    for (const r of result) {
+      if (equals(r, item)) {
+        // 外层循环继续下一次
+        continue outer
+      }
+    }
+    result.push(item)
+  }
+  return result
+}
+
+console.log(fn2(arr))
