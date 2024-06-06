2dfdb3b8f383337919dc1a79188c697f4a343c10
189690 <yuhang.ma@cn.chervongroup.com>
2024-06-06 16:22:05 +0800

    fix：其他16

--- 其他/16-判断对象中某个属性是否存在.js
+++ 其他/16-判断对象中某个属性是否存在.js
@@ -1,5 +1,9 @@
+const symbol = Symbol('key')
+
+
 const obj = {
   a: 1,
+  [symbol]: 2
 }
 
 Object.defineProperty(obj, 'b', {
@@ -10,10 +14,10 @@ Object.defineProperty(obj, 'b', {
 obj.__proto__.c = 3
 
 // console.log('c' in obj); // 原型上的、自身的所有属性 底层使用的Reflect.has()
-// console.log(obj.hasOwnProperty('b')); // 自身的所有属性
-console.log(Object.keys(obj));   // 不包括原型上的、自身可遍历的属性
-// 自身和原型上的可遍历属性
+// console.log(obj.hasOwnProperty(symbol)); // 自身的所有属性
+// console.log(Object.keys(obj));   // 不包括原型上的、自身可遍历的属性(不包括symbol类型)
+// // 自身和原型上的可遍历属性（不包括symbol）
 // for (const key in obj) {
 //   console.log(key);
 // }
-// console.log(Reflect.ownKeys(obj)); // 不包括原型上的、自身所有的属性
+console.log(Reflect.ownKeys(obj)); // 不包括原型上的、自身所有的属性（包括symbol）
