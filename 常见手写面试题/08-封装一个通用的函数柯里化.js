df6a21b40889207f4c9a7dd9f39e286d4d2ca077
189690 <yuhang.ma@cn.chervongroup.com>
2024-09-05 15:46:01 +0800

    feat: 增加函数柯里化的第二种写法

--- 常见手写面试题/08-封装一个通用的函数柯里化.js
+++ 常见手写面试题/08-封装一个通用的函数柯里化.js
@@ -2,23 +2,31 @@ function add(a, b, c, d) {
   return a + b + c + d
 }
 
-function curry(fn, ...args) {
-  const data = [...args]
-  if (data.length === fn.length) {
-    // 说明参数已经够了
-    return fn.apply(this, data)
-  }
-  function _curry(...rest) {
-    data.push(...rest)
-    if (data.length === fn.length) {
-      return fn.apply(this, data)
-    }
-    return _curry
-  }
+// function curry(fn, ...args) {
+//   const data = [...args]
+//   if (data.length === fn.length) {
+//     // 说明参数已经够了
+//     return fn.apply(this, data)
+//   }
+//   function _curry(...rest) {
+//     data.push(...rest)
+//     if (data.length === fn.length) {
+//       return fn.apply(this, data)
+//     }
+//     return _curry
+//   }
 
-  return _curry
+//   return _curry
+// }
+function curry(fn, ...args) {
+  if (args.length === fn.length) {
+    return fn.apply(this, args)
+  }
+  return function (...rest) {
+    return curry(fn, ...args, ...rest)
+  }
 }
 
-console.log(curry(add, 1, 2, 3, 4));
-console.log(curry(add, 1, 2)(3, 4));
-console.log(curry(add)(1)(2)(3)(4));
+console.log(curry(add, 1, 2, 3, 4))
+console.log(curry(add, 1, 2)(3, 4))
+console.log(curry(add)(1)(2)(5)(4))
