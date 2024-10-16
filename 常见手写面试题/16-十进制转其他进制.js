d89c32d24f530cdc76df7a24ec4db8554893008e
189690 <yuhang.ma@cn.chervongroup.com>
2024-10-16 16:44:01 +0800

    fix: 十进制转成其他进制

--- 常见手写面试题/16-十进制转其他进制.js
+++ 常见手写面试题/16-十进制转其他进制.js
@@ -1,7 +1,7 @@
 // 十进制转成其他进制
 // num 十进制的数
 // n进制
-function tenToTwo(num, n) {
+function tenToOther(num, n) {
   if (typeof num !== 'number') return
   let res = []
   while (num > 0) {
@@ -10,4 +10,4 @@ function tenToTwo(num, n) {
   }
   return res.join('')
 }
-console.log(tenToTwo(7, 8))
+console.log(tenToOther(10, 8))
