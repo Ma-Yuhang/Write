87e5c385917040b9d3f01682693b7c5319a72a3f
189690 <yuhang.ma@cn.chervongroup.com>
2024-10-18 16:49:53 +0800

    fix: 优化判断对象中的属性的方法

--- 其他/16-判断对象中某个属性是否存在.js
+++ 其他/16-判断对象中某个属性是否存在.js
@@ -1,23 +1,33 @@
 const symbol = Symbol('key')
 
-
 const obj = {
   a: 1,
-  [symbol]: 2
+  [symbol]: 2,
 }
 
 Object.defineProperty(obj, 'b', {
   value: 2,
-  enumerable: false
+  enumerable: false,
 })
 
 obj.__proto__.c = 3
 
-// console.log('c' in obj); // 原型上的、自身的所有属性 底层使用的Reflect.has()
-// console.log(obj.hasOwnProperty(symbol)); // 自身的所有属性
-// console.log(Object.keys(obj));   // 不包括原型上的、自身可遍历的属性(不包括symbol类型)
-// // 自身和原型上的可遍历属性（不包括symbol）
+// 可以判断 原型上的、自身的 所有属性中某个属性是否存在 Reflect.has()
+console.log('c' in obj, 'in操作符');
+
+// 可以判断 自身的 所有属性中某个属性是否存在（Object.hasOwn() 取代 hasOwnProperty）
+console.log(Object.hasOwn(obj, symbol), 'Object.hasOwn()');
+
+// 自身的 可遍历属性，不包括symbol类型
+console.log(Object.keys(obj), 'Object.keys()');
+
+// 自身和原型上的 可遍历属性，不包括symbol类型
 // for (const key in obj) {
 //   console.log(key);
 // }
-console.log(Reflect.ownKeys(obj)); // 不包括原型上的、自身所有的属性（包括symbol）
+
+// 自身所有的属性（包括symbol）
+console.log(Reflect.ownKeys(obj), 'Reflect.ownKeys()')
+
+// 自身的 除了symbol类型以外的 所有属性
+console.log(Object.getOwnPropertyNames(obj), 'Object.getOwnPropertyNames()')
