3a46e9d7a4d3e3fafc76cfe62432839f945585f6
189690 <yuhang.ma@cn.chervongroup.com>
2025-02-05 15:33:00 +0800

    fix: 修改手写instanceof

--- 常见手写面试题/01-手写instanceof.js
+++ 常见手写面试题/01-手写instanceof.js
@@ -4,15 +4,14 @@ console.log(arr instanceof RegExp)
 console.log(arr instanceof Array)
 console.log('s' instanceof Object)
 function myInstanceof(example, type) {
-  let typePrototype = type.prototype
+  // 如果不是对象或函数，直接返回 false（原始值不在原型链上）
+  if (example === null || typeof example !== 'object') return false
+  const typePrototype = type.prototype
   let proto = Object.getPrototypeOf(example)
   while (proto) {
     if (proto === typePrototype) {
       return true
     }
-    // if (proto === null) {
-    //     return false
-    // }
     proto = Object.getPrototypeOf(proto)
   }
   return false
